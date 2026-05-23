43392793fa65f10e1c1102ccce2c663e150ec31c
Cosmic <cosmicz211407@gmail.com>
2026-05-23 20:00:20 +0530

    Add: matrix-diagonal-sum - submission-0

---
+++ Data Structures & Algorithms/matrix-diagonal-sum/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        su = 0
+        for i in range(n):
+            su += mat[i][i]          
+            su += mat[i][n - 1 - i]   
+        if n % 2 == 1:
+            su -= mat[n // 2][n // 2]
+
+        return su
