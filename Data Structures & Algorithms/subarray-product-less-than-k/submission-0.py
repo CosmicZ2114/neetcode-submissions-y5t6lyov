522915fc92952eb2a48c05d065f1a08f371d16c9
Cosmic <cosmicz211407@gmail.com>
2026-06-12 18:01:27 +0530

    Add: subarray-product-less-than-k - submission-0

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k<=1:
+            return 0
+        l=0
+        r=0
+        c=0
+        p=1
+        while r<len(nums):
+            p *= nums[r]
+            while p >= k:
+                p //= nums[l]
+                l += 1
+            c += r - l + 1
+            r+=1
+        return c
